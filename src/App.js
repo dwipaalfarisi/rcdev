789ceb5f24dd75675b088ceb35fbd323c4a29122
dwipaalfarisi <dwipa.alfarisi@gmail.com>
2023-07-05 18:00:23 +0700

    try displaying data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import React from "react";
+import "./App.css";
+
+const user = {
+  name: "avatar",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
+
 function MyButton() {
   return <button>Click me!</button>;
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About Page</h1>
+      <p>This is the about page</p>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
+  );
 }
 
 export default function MyApp() {
@@ -7,6 +34,7 @@ export default function MyApp() {
     <div>
       <h1>My App</h1>
       <MyButton />
+      <AboutPage />
     </div>
   );
 }
